ec30cca61775043ec70d0c113464c915a2d049ea
Ido Strauss <strauss02@hotmail.co.il>
2021-10-18 19:19:57 +0300

    select elements

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
+const infoName = document.querySelector('.name');
+const infoWeight = document.querySelector('.weight');
+const infoHeight = document.querySelector('.height');
+let infoType = document.querySelector('.type-list');
+const infoImg = document.querySelector('.info-image');
+const morePokemonsList = document.querySelector('.pokemons-of-type');
+
 const searchInput = document.querySelector('.search');
 
 const searchBtn = document.querySelector('.search-btn');
 
 searchBtn.addEventListener('click', searchPokemon);
+infoImg.addEventListener('mouseover', showBack);
+infoImg.addEventListener('mouseleave', showFront);
+infoType.addEventListener('click', showTypeList);
+morePokemonsList.addEventListener('click', handleListClick);
 
 async function searchPokemon() {
   const pokeName = searchInput.value;
   await axios
     .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
-    .then((res) => (pokeData = res.data));
-
-  updateData();
+    .then((res) => {
+      pokeData = res.data;
+      updateData();
+    })
+    .catch((err) => inform(err));
 }
 
 let pokeData = {};
-
-const infoName = document.querySelector('.name');
-const infoWeight = document.querySelector('.weight');
-const infoHeight = document.querySelector('.height');
-const infoType = document.querySelector('.type');
 
 function updateData() {
   infoName.innerText = pokeData.name;
   infoWeight.innerText = pokeData.weight;
   infoHeight.innerText = pokeData.height;
-  infoType.innerText = getTypes();
-  console.log(pokeData.sprites.front_default);
-  console.log(pokeData.sprites.back_default);
+  getTypes();
+  infoImg.src = pokeData.sprites.front_default;
 }
 
 function getTypes() {
