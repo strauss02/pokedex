9275d1e25374a3a41526f9db2a449c46a6f559f4
Ido Strauss <strauss02@hotmail.co.il>
2021-10-18 20:51:27 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -109,15 +109,12 @@ var btn = document.getElementById('myBtn');
 var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = showModal;
 
 function showModal() {
   modal.style.display = 'block';
 }
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  closeModal();
-};
+span.addEventListener('click', closeModal);
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = outsideClick;
